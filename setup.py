16aa44ec6333f24fba9905e36a72c1b704872912
DiogoAmaro13 <diogoaia10@gmail.com>
2025-07-11 02:12:56 +0100

    updated files

--- setup.py
+++ setup.py
@@ -2,10 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name="gpr_modelling",
-    version="0.1",
-    description="Gaussian Process Regression Modeling Toolkit",
+    version="0.1.0",
     author="Diogo Amaro",
+    description="Gaussian Process Regression Toolkit for Cardiac Statistical Emulation",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[],  # Add required dependencies here later, like 'numpy', 'scikit-learn', etc.
+    install_requires=[
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
 )
+
